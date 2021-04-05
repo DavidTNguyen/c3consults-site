a9e1df445356ce3d1a30ce4ef3515ad4bc0ace3f
David.T.Nguyen <david.t.nguyen@target.com>
2021-04-05 15:59:05 -0500

    Add video to collection

--- src/components/videos/videos.js
+++ src/components/videos/videos.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
-import { config } from 'react-awesome-styled-grid'
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
-// import { videos } from '../../../data/videos'
+import siteConfig from '../../../data/siteConfig'
 
 const YoutubeSlide = ({ url, isSelected }) => (
-  <ReactPlayer width="100%" url={url} playing={isSelected} />
+  <ReactPlayer width="100%" height="500px" url={url} playing={isSelected} />
 )
 
 const Videos = ({ className }) => {
@@ -21,20 +20,25 @@ const Videos = ({ className }) => {
   const getVideoId = url =>
     url.substr('https://www.youtube.com/embed/'.length, url.length)
 
-  const customRenderThumb = children =>
+  const customRenderThumb = (children) =>
     children.map(item => {
       const videoId = getVideoId(item.props.url)
       return <img src={getVideoThumb(videoId)} />
     })
 
   return (
-    <div className="player-wrapper">
-      <ReactPlayer
-        playing
-        className="react-player"
-        width="100%"
-        url="https://www.youtube.com/watch?v=9LAVC2kUOzo"
-      />
+    <div className={className}>
+      <div className="player-wrapper">
+        <Carousel
+          renderItem={customRenderItem}
+          renderThumbs={customRenderThumb}
+        >
+          {siteConfig.videos &&
+            siteConfig.videos.map(vid => (
+              <YoutubeSlide key={vid.key} url={vid.url} />
+            ))}
+        </Carousel>
+      </div>
     </div>
   )
 }
@@ -43,6 +47,7 @@ export default styled(Videos)`
   position: relative;
   margin: auto;
   width: 100%;
+  height:100%;
   background: ${({ theme }) => theme.colors.primary};
 
   .carousel-background {
@@ -51,7 +56,7 @@ export default styled(Videos)`
 
   .player-wrapper {
     position: relative;
-    padding-top: 56.25%; /* 720 / 1280 = 0.5625 */
+    // padding-top: 56.25%; /* 720 / 1280 = 0.5625 */
   }
 
   .react-player {
