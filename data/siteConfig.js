821299532fc89f79148694f5a9e5fc6e2050d97a
David.T.Nguyen <david.t.nguyen@target.com>
2021-11-15 14:03:43 -0600

    Fix email

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -152,7 +152,7 @@ module.exports = {
   ],
   social: {
     linkedin: 'https://www.linkedin.com/in/khanh-nguyen-18838830/',
-    email: 'mailto://c3consults@gmail.com',
+    email: 'mailto:c3consults@gmail.com',
   },
   siteUrl: 'https://github.com/DavidTNguyen/c3consults-site',
   pathPrefix: '/c3consults-site', // Note: it must *not* have a trailing slash.
