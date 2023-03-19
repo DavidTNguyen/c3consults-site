0971a399a67c3714788b2029c846bcce6b50b22a
Nguyen <davidnguy@gmail.com>
2023-03-18 19:57:16 -0500

    Upgrade to React 18 and Gatsby 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   pathPrefix: config.pathPrefix,
   plugins: [
+    `gatsby-plugin-pnpm`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
