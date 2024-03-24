76e3d70f6d5491e18b110c5af5dd465f9b64d359
David Nguyen <davidnguy@gmail.com>
2024-03-24 16:03:54 -0500

    Update Packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-material-ui`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
